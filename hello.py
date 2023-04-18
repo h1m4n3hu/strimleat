53e6ea82218186e03542c0f6dc11dc2899ecb04e
Himanshu Soni <57286608+h1m4n3hu@users.noreply.github.com>
2023-04-18 23:53:38 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write('Hello world!')
+st.write('Enter Three Numbers')
+
+st.number_input('Enter 1st No.')
+st.number_input('Enter 2nd No.')
+st.number_input('Enter 3rd No.')
