cd6c194bd07bee437b7fd946b38a7a9feb6a9e2b
Himanshu Soni <57286608+h1m4n3hu@users.noreply.github.com>
2023-04-18 23:56:30 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.write('Enter Three Numbers')
 
-st.number_input('Enter 1st No.')
-st.number_input('Enter 2nd No.')
-st.number_input('Enter 3rd No.')
+a=st.number_input('Enter 1st No.')
+b=st.number_input('Enter 2nd No.')
+c=st.number_input('Enter 3rd No.')
+
+st.write(max(a,b,c))
